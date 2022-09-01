3b491b57af07a895e2af9d0f6b61e1a56723e7ef
Jakub Pietrus <pietrusjakub@gmail.com>
2022-09-01 17:21:51 +0200

    Finish recursion section

--- recursion.py
+++ recursion.py
@@ -40,3 +40,64 @@ def gcd(a, b):
     if b == 0:
         return a
     return gcd(b, a % b)
+
+
+def decimalToBinary(n):
+    assert int(n) == n, 'The number must be positive integer'
+    if n == 0:
+        return 0
+    return n % 2 + 10 * decimalToBinary(int(n//2))
+
+
+def flatten(arr):
+    resultArr = []
+    for custItem in arr:
+        if type(custItem) is list:
+            resultArr.extend(flatten(custItem))
+        else:
+            resultArr.append(custItem)
+    return resultArr
+
+
+def capitalizeFirst(arr):
+    result = []
+    if len(arr) == 0:
+        return result
+    result.append(arr[0][0].upper() + arr[0][1:])
+    return result + capitalizeFirst(arr[1:])
+
+
+def nestedEvenSum(obj, sum=0):
+    for key in obj:
+        if type(obj[key]) is dict:
+            sum += nestedEvenSum(obj[key])
+        elif type(obj[key]) is int and obj[key] % 2 == 0:
+            sum += obj[key]
+    return sum
+
+
+def capitalizeWords(arr):
+    result = []
+    if len(arr) == 0:
+        return result
+    result.append(arr[0].upper())
+    return result + capitalizeWords(arr[1:])
+
+
+def stringifyNumbers(obj):
+    for key in obj:
+        if type(obj[key]) is dict:
+            stringifyNumbers(obj[key])
+        elif type(obj[key]) is int:
+            obj[key] = str(obj[key])
+    return obj
+
+
+def collectStrings(obj):
+    result = []
+    for key in obj:
+        if type(obj[key]) is dict:
+            result += collectStrings(obj[key])
+        elif type(obj[key]) is str:
+            result.append(obj[key])
+    return result
